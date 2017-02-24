2c090a04daf4f8c43eed1986115a7902af388275
Alexander Chiu <al.chiu407@gmail.com>
2017-02-24 14:12:47 -0800

    Added new node modules, modified main .js, added test .js, and added new templates

--- meadowlark.js
+++ meadowlark.js
@@ -4,22 +4,92 @@ var fortune = require('./lib/fortune.js');
 var app = express();
 
 // Set up handlebars view engine
-var handlebars = require('express3-handlebars').create({defaultLayout:'main'});
+var handlebars = require('express3-handlebars').create({
+	defaultLayout:'main', 
+	helpers:{
+		section: function(name, options){
+			if(!this._sections) this._sections = {};
+			this._sections[name] = options.fn(this);
+			return null;
+			}
+		}
+	}
+);
 app.engine('handlebars', handlebars.engine);
 app.set('view engine', 'handlebars');
 
+// Set the listening port to 3000
 app.set('port', process.env.PORT || 3000);
 
+// Express will pull all public files from /public
 app.use(express.static(__dirname+ '/public'));
+
+// Allow testing when a certin condition is met
+app.use(function(req, res, next){
+	res.locals.showTests = app.get('env') !== 'production' && req.query.test ==='1';
+	next();
+});
+
+function getWeatherData(){
+	return {
+		locations: [
+			{
+				name: 'Portland',
+				forecastURL: 'http://www.wunderground.com/US/OR/Portland.html',
+				iconURL: 'http://icons-ak.wxug.com/i/c/k/cloudy.gif',
+				weather: 'Overcast',
+				temp: '54.1 F (12.3 C)',
+			},
+			{
+				name: 'Bend',
+				forecastURL: 'http://www.wunderground.com/US/OR/Bend.html',
+				iconURL: 'http://icons-ak.wxug.com/i/c/k/partlycloudy.gif',
+				weather: 'Partly Cloudy',
+				temp: '55.0 F (12.8 C)',			
+			},
+			{
+				name: 'Manzanita',
+				forecastURL: 'http://www.wunderground.com/US/OR/Manzanita.html',
+				iconURL: 'http://icons-ak.wxug.com/i/c/k/rain.gif',
+				weather: 'Light Rain',
+				temp: '55.0 F (12.8 C)'
+			},
+		],
+	};
+}
+
+app.use(function(req, res, next){
+	if(!res.locals.partials) res.locals.partials = {};
+	res.locals.partials.weather = getWeatherData();
+	next();
+});
 
 // Homepage
 app.get('/', function(req, res){
 	res.render('home');
 });
 
+// JqueryTest
+app.get('/JqueryTest', function(req,res){
+	res.render('jquery-test');
+});
+
 // About Page
 app.get('/about', function(req, res){
-	res.render('about', {fortune: fortune.getFortune()});
+	res.render('about', {
+		fortune: fortune.getFortune(),
+		pageTestScript: '/qa/tests-about.js'
+	});
+});
+
+// Hood River Page
+app.get('/tours/hood-river', function(req, res){
+	res.render('tours/hood-river');
+});
+
+// Request group rate page
+app.get('/tours/request-group-rate', function(req, res){
+	res.render('tours/request-group-rate');
 });
 
 // Custom 404 catch-all handler (middleware)
